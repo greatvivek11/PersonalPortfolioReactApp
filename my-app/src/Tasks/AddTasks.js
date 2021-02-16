ed6d6fe7f56deec4d9c01c18c8d52ebb15e4a27d
greatvivek11 <vcoolkaushik@gmail.com>
2021-02-16 13:08:22 +0530

    Fixed input field bug preventing adding new tasks.

--- my-app/src/Tasks/AddTasks.js
+++ my-app/src/Tasks/AddTasks.js
@@ -4,26 +4,30 @@ import { Button } from 'react-bootstrap';
 
 export default function AddTasks(props) {
     const [Tasks, setTasks] = useState();
-    const [inputValue, setInputValue] = useState();
     // console.log(props.tasks);
 
     function onClickAddTask(){
         updateTasks(Tasks);
         // console.log(Tasks);
         props.onChange(Tasks);
-        setInputValue("");
     }
 
     function onInputChange(e){
         // console.log(e.target.value);
         setTasks(props.tasks?.tasks?.concat({task:e.target.value}));
     }
+    function onSubmit(e){
+        e.preventDefault();
+        e.target.reset();
+    }
 
     return (
         <div>
+            <form onSubmit={onSubmit}>
             <label>Add Task:</label>
-            <input style={{marginLeft: '0.5rem',margin:'0.5rem'}} value={inputValue} type ="text" name="addTask" placeholder="Add a Task" onChange={onInputChange}></input>
-            <Button style={{marginLeft: '0.5rem',margin:'0.5rem',backgroundColor:'green'}} name="addTask" onClick={onClickAddTask}>Add Task</Button>
+            <input style={{marginLeft: '0.5rem',margin:'0.5rem'}} type ="text" name="addTask" placeholder="Add a Task" onChange={onInputChange}></input>
+            <Button type="submit"style={{marginLeft: '0.5rem',margin:'0.5rem',backgroundColor:'green'}} name="addTask" onClick={onClickAddTask}>Add Task</Button>
+            </form>
         </div>
     )
 }
