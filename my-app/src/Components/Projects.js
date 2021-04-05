3afa96a5ce408b0f9b07a3ee656d984fd49c7887
greatvivek11 <vcoolkaushik@gmail.com>
2021-04-05 17:52:04 +0530

    Changed Project Name only.

--- my-app/src/Components/Projects.js
+++ my-app/src/Components/Projects.js
@@ -3,7 +3,7 @@ import AccordianComponent from './AccordianComponent';
 import { Project1,Project2,Project3,Project4,Project5} from "./strings";
 
 export default function Projects() {
-    const dict = {"Personal Portfolio Project":Project1,"Boston Scientific C#/Java Project":Project2,"Epredia Cloud/IoT Project":Project3,"Epredia Android Project":Project4,"PGI Java/MySQL Project":Project5}
+    const dict = {"Personal Portfolio Project":Project1,"Boston Scientific C#/Java Project":Project2,"Epredia Cloud/IoT Project":Project3,"Ascensia Android Project":Project4,"PGI Java/MySQL Project":Project5}
     const style = {"color1":"hotpink","color2":"pink"};
     return (
         <div className="UnderConstruction">
