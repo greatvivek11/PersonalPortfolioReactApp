5525ea5eb652716e0efc03154f9f25c1051d9195
greatvivek11 <vcoolkaushik@gmail.com>
2022-02-12 00:16:52 +0530

    sonarcloud fix - added https to blog link.

--- my-app/src/Components/Blog.jsx
+++ my-app/src/Components/Blog.jsx
@@ -6,7 +6,7 @@ export default function Blog() {
             <h1>Blogs</h1>
             <p>{Blog1}</p>
             <p>{Blog2}</p>
-            <p>Meanwhile, follow my Wordpress blog here - <a target="_blank"  href="http://vivekkaushikblog.wordpress.com/">My Blog</a></p>
+            <p>Meanwhile, follow my Wordpress blog here - <a target="_blank"  href="https://vivekkaushikblog.wordpress.com/">My Blog</a></p>
         </div>
     )
 }
