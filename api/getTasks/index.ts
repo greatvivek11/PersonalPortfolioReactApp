b9c7df19c4bfced6a7b10ee538537c6304e49750
Vivek Kaushik <54403368+greatvivek11@users.noreply.github.com>
2022-04-03 18:05:13 +0530

    Moved Apis from cosmosdb to blob storage.
    * Moved Apis from cosmosdb to blob storage.
    
    * Modified endpoints in react app

--- api/getTasks/index.ts
+++ api/getTasks/index.ts
@@ -1,15 +1,11 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, getTasks: Document): Promise<void> {
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const output:Tasks = {tasks:getTasks["tasks"]};
-    const responseMessage = output
-        ? output
-        : "Tasks are empty";
-
+    const tasks:Tasks = context.bindings.tasksBlob;
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: tasks
     };
 
 };
