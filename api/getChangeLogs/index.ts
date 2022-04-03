b5ee538d1f35db228a6445c82a36c23bde89b6bf
greatvivek11 <vcoolkaushik@gmail.com>
2022-04-03 17:40:41 +0530

    Moved Apis from cosmosdb to blob storage.

--- api/getChangeLogs/index.ts
+++ api/getChangeLogs/index.ts
@@ -1,15 +1,11 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest,  getChangeLogs: Document): Promise<void> {
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const output:ChangeLogs = {ChangeLog:getChangeLogs["ChangeLog"]};
-    const responseMessage = output
-        ? output
-        : "Tasks are empty";
-
+    const tasks:Tasks = context.bindings.tasksBlob;
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: tasks
     };
 
 };
