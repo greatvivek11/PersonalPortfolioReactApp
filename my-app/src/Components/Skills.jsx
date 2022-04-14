c75600107ef158d74c2a36362392c62234c634f8
Vivek Kaushik <54403368+greatvivek11@users.noreply.github.com>
2022-04-14 12:01:54 +0530

    Fix/modified todo links bump versioning (#15)
    * Fix -
    1. Modified todo app link
    2. Changed workflows order :
    bump ver on the push at master-> build and deploy master

--- my-app/src/Components/Skills.jsx
+++ my-app/src/Components/Skills.jsx
@@ -36,7 +36,7 @@ function Skills() {
                     <li>{Headline2}</li>
                     <li>{Headline3}</li>
                     <li>{Headline4}</li>
-                    <li>{Headline5}<a href="https://todo.vivekkaushik.in" target="_blank" rel="noreferrer">ToDoApp</a></li>
+                    <li>{Headline5}<a href="https://todo.vivekkaushik.co.in" target="_blank" rel="noreferrer">ToDoApp</a></li>
                 </div>
                 <h1>Skills</h1>
                 <Suspense fallback={<div>Loading...</div>}>
