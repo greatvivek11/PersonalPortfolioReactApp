2ef0b7b1595e6506ccc191313e870134b3cffd34
greatvivek11 <vcoolkaushik@gmail.com>
2024-01-20 23:57:54 +0530

    deloitte ui changes

--- my-app/src/Components/Projects.jsx
+++ my-app/src/Components/Projects.jsx
@@ -1,13 +1,13 @@
 import { Container } from 'react-bootstrap';
 import { lazy, Suspense } from 'react';
-import { Project1, Project2, Project3, Project4, Project5, Project6, Project7 } from "./strings.jsx";
+import { Project1, Project2, Project3, Project4, Project5, Project6, Project7, Project8 } from "./strings.jsx";
 const AccordianComponent = lazy(() => import('./AccordianComponent'));
 
 export default function Projects() {
-    const dict = {"Axis Capital": Project7, "Todo App": Project6, "Personal Portfolio": Project1, "Boston Scientific": Project2, "Epredia": Project3, "Ascensia": Project4, "PGI Chandigarh": Project5 }
+    const dict = { "Kenvue (formerly J&J)": Project8, "Axis Capital": Project7, "Todo App": Project6, "Personal Portfolio": Project1, "Boston Scientific": Project2, "Epredia": Project3, "Ascensia": Project4, "PGI Chandigarh": Project5 }
     const style = { "color1": "hotpink", "color2": "pink" };
     return (
-        <div className="UnderConstruction" style={{paddingTop:"8%"}}>
+        <div className="UnderConstruction" style={{ paddingTop: "8%" }}>
             <Suspense fallback={<div>Loading...</div>}>
                 <Container>
                     <AccordianComponent props={dict} styles={style} />
